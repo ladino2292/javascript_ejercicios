94d2313922418357a12c5a32f83048d0264c94eb
David Camilo <56163983+ladino2292@users.noreply.github.com>
2024-03-08 09:29:40 -0500

    Add files via upload

---
+++ pagina/template/cabecera.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTRO DE USUARIOS</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+
+</head>
+<body>
+
+    <nav class="navbar navbar-expand navbar-light bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">INTENTO <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Estudiantes</a>
+            </li>
+        </ul>
+    </nav>
+<div class="container">
+    <div class="row">
+</body>
+</html>
